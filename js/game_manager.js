d85671d8f1254badbe27bbd18a5b2d008a5592ba
Todd Cesere <tcesere@greenriver.org>
2014-04-27 18:13:35 -0400

    Added random 4s back in and changed starting size to 2x2.

--- js/game_manager.js
+++ js/game_manager.js
@@ -70,7 +70,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 2;
+    var value = Math.random() < 0.9 ? 2 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
     if (tile.value == this.nextExpander) {
       tile.expander = true;
